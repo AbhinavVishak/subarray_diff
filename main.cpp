b8ce242d5048482de6bd49d74cba06fbd8218c54
Abhinav Vishak Iyappan <Abhinav Vishak Iyappan>
2017-03-17 18:48:23 -0500

    Completes the main class

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include<iostream>
+#include<fstream>
+#include<vector>
+
+int calculate( std::vector<int> &d , int k ) 
+{
+
+}
 
 int main()
 {
-    bool a ; 
-    std::cout << sizeof(a) ; 
+    std::ifstream inp ; 
+    inp.open("input.txt") ; 
+    int n,k ; 
+    
+    if( inp.is_open()  )
+    {
+        inp >> n >> k ;  
+    }
+    else 
+    {
+        std::cout << "FILE WAS NOT OPENED . TERMINATING PROGRAM."  ; 
+        return 0 ; 
+    }
+    std::vector<int> nums(n) ;
+    std::vector<int> diff(n-1) ;
+    for( int i = 0 ; i < nums.size() ; i++ )
+            inp >> nums[i] ; 
+    inp.close() ; 
+    for( int i = 0 ; i < nums.size()-1 ; i++ )
+    {
+        if( nums[i] < nums[i+1] ) diff[i] = 1 ; 
+        else if ( nums[i] > nums[i+1] ) diff[i] = -1 ; 
+        else diff[i] = 0 ; 
+    }
+    calculate( diff , k) ; 
 }
