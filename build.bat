919ee119def3c777d63a974b7a8575be688618d7
Abhinav Vishak Iyappan <Abhinav Vishak Iyappan>
2017-03-19 16:27:56 -0500

    commits all

--- build.bat
+++ build.bat
@@ -1,5 +1,4 @@
 @echo on
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64     
 set compilerflags=/O1 /EHsc
-set linkerflags=/OUT:program.exe
 cl.exe %compilerflags% main.cpp /link %linkerflags%
